f70d5f85d94f85c57dac60db34790ec928c409a3
exist_52 <Galo_dev_art@proton.me>
2026-07-09 07:35:20 +0000

    that part at the second top

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex p-4 justify-center">
+    <div className="flex p-2 justify-center border-b border-gray-200">
       {/*Big div wrapper */}
       <Image
         src={"/images/gromuse.png"}
@@ -11,9 +11,9 @@ export default function Home() {
         width={180}
         className="inline"
       />
-      <div className="w-[50%] h-12 bg-amber-200 border border-gray-300">
+      <div className="w-[50%] h-12 bg-amber-200 border rounded-r-xl border-gray-300 ml-15">
         {/*searchbar div */}
-        <span className="border-r h-full border-gray-900">
+        <span className="border-r rounded-l-3xl border w-full h-full border-gray-900">
           <Image
             src={"pin.svg"}
             height={20}
@@ -30,36 +30,41 @@ export default function Home() {
               className="w-[77%] h-full border-none"
             ></input>
           </span>
-          <span className="bg-primaryg h-fit w-fit">
+          <span className="bg-primaryg bg-cover h-[50%] w-[50%]">
             <Image
               src={"search.svg"}
               height={20}
               width={25}
               alt="pin"
-              className="inline"
+              className="inline bg-cover"
             />
           </span>
         </span>
       </div>
-      <div>
+      <div className="px-4 py-3 ml-2">
         <span>
           <span className="p-1">
             <Image
               src={"cart.svg"}
               height={15}
-              width={20}
+              width={26}
               alt="pin"
               className="inline"
             />
           </span>
-          <span>Cart</span>
+          <span className="relative">
+            Cart
+            <span className="absolute text-sm mb-4 p-1 bg-secondaryg rounded-4xl w-4 h-4 text-white text-center ">
+              1
+            </span>
+          </span>
         </span>
-        <span>
+        <span className="ml-3">
           <span className="p-1">
             <Image
               src={"person.svg"}
               height={15}
-              width={20}
+              width={26}
               alt="pin"
               className="inline"
             />
